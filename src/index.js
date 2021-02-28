68001dbb1c84fd518bd85786242568ab82a6118d
4ebula <4ebula@gmail.com>
2021-02-28 21:04:39 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.lenght & 1) return false;
+  let position;
+  let switcher = true;
+  while(switcher){
+    switcher = false;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      while (true) {
+        position = str.indexOf(bracketsConfig[i].join(''));
+        if (~position) {
+          str = str.substr(0, position) + str.substr(position + 2, str.length);
+          switcher = true;
+        }
+        else break;
+      }
+    }
+  }
+  return !str.length
+
 }
